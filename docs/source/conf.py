591bcf35882d61a3c7ffdedf7121ee789d527ad6
CandyBot <90443263+CandyBotDJS@users.noreply.github.com>
2021-10-09 10:07:26 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PersianDevs'
+copyright = '2021, PersianDevs team'
+author = 'Parsa'
 
 release = '0.1'
 version = '0.1.0'
